8a2e0ab6610bb6dde00d84b7e8efd58aad0101f5
Svpernova09 <halo09@gmail.com>
2013-07-17 22:13:23 -0500

    Adding shell provisioning files

---
+++ vagrant-talk-1/shell-provisioning.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get -y install python-software-properties
+sudo apt-get purge openjdk*
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+sudo apt-get -y install oracle-java7-installer
+
+
+
+
+
+
+
