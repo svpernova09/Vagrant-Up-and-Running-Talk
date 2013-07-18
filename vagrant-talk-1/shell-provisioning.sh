0ed44ebb16095aca7a5f4a517c5b654594e517c0
Svpernova09 <halo09@gmail.com>
2013-07-18 11:45:36 -0500

    Causing Trouble

--- vagrant-talk-1/shell-provisioning.sh
+++ vagrant-talk-1/shell-provisioning.sh
@@ -4,8 +4,8 @@ sudo apt-get -y install python-software-properties
 sudo apt-get purge openjdk*
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
-echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+#echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+#echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 sudo apt-get -y install oracle-java7-installer
 
 
