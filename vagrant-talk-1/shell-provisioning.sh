8b172d3c91c8190540fc8704b9702c8977bf8b1e
Svpernova09 <halo09@gmail.com>
2013-07-23 09:29:26 -0500

    Tweaking for PHP Audience

--- vagrant-talk-1/shell-provisioning.sh
+++ vagrant-talk-1/shell-provisioning.sh
@@ -2,11 +2,12 @@
 sudo apt-get update
 sudo apt-get -y install python-software-properties
 sudo apt-get purge openjdk*
-sudo add-apt-repository ppa:webupd8team/java
+#sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 #echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
 #echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-sudo apt-get -y install oracle-java7-installer
+#sudo apt-get -y install oracle-java7-installer
+sudo apt-get -y install vim git php5-cli php5-mysql php5-mcrypt php5-intl php5-sqlite
 
 
 
