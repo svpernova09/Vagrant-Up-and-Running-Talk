9b981416120ab77d512eb94ccb0ecb8452ba35cf
Svpernova09 <halo09@gmail.com>
2013-09-12 08:48:25 -0500

    Working on talk-1 for Python

--- vagrant-talk-1/shell-provisioning.sh
+++ vagrant-talk-1/shell-provisioning.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get -y install python-software-properties
-sudo apt-get purge openjdk*
-#sudo add-apt-repository ppa:webupd8team/java
+#sudo apt-get purge openjdk* # ELOHEL Java
+#sudo add-apt-repository ppa:webupd8team/java # ELOHEL Java
 sudo apt-get update
-#echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-#echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-#sudo apt-get -y install oracle-java7-installer
-sudo apt-get -y install vim git php5-cli php5-mysql php5-mcrypt php5-intl php5-sqlite
-
-
+#echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections # ELOHEL Java
+#echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections # ELOHEL Java
+#sudo apt-get -y install oracle-java7-installer # ELOHEL Java
+#sudo apt-get -y install vim git php5-cli php5-mysql php5-mcrypt php5-intl php5-sqlite # ELOHEL PHP
+sudo apt-get -y install python-pip screen
+sudo pip install Django
+django-admin.py startproject mysite
+screen -dmS django-run sh
+screen -S django-run -p 0 -X stuff "cd mysite && python manage.py runserver 0.0.0.0:8000
+"
 
 
 
